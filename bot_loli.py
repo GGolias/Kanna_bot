0f4dba61e8e5c54f7e84190087885c91127d9b4d
carlosneto726 <carlosneto726@gmail.com>
2022-04-10 16:06:20 -0300

    Implemetei a conversação e entrar e sair de canais de voz

--- bot_loli.py
+++ bot_loli.py
@@ -1,28 +1,104 @@
+# Importando Bibliotecas
 import discord
 import os
+import requests
 from dotenv import load_dotenv, find_dotenv
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+from time import sleep
 
+# Carrega o arquivo .env
 load_dotenv(find_dotenv())
 
-client = commands.Bot(command_prefix='$')
+intents = discord.Intents.default()
+intents.members = True
 
+# Define o prefixo dos comandos
+client = commands.Bot(command_prefix='!!', intents = intents)
+
+# ==================================== Eventos ====================================
+
+# Status do Bot
 @client.event
 async def on_ready():
     print("Logamos com sucesso como {0.user}".format(client))
 
+# Efetua boas vindas
+@client.event
+async def on_member_join(member):
+  channel = client.get_channel(961343761986764943)
+  await channel.send("Seja bem Vindo(a)! Me ajude a sair do porão!")
+
+# Se despede 
+@client.event
+async def on_member_remove(member):
+  channel = client.get_channel(961343761986764943)
+  await channel.send("Espero que tenha gostado de tomar uma breja comigo... eh digo suco hehe")
+
+# ==================================== Comandos ====================================
+
+# Listar comandos
 @client.command()
 async def comandos(ctx):
-  await ctx.send('**COMANDOS** \n$play \n$gemido \n$conversa \n$meme \n$loli')
+  await ctx.send('**COMANDOS** \n!!ohayo - dou um bom dia bem animado pra você! \n!!gemido - vou gemer no seu ouvido senpai-kun.\n!!loli - vou conversar com você até altas horas.')
 
+# Falar com o Bot
 @client.command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
+async def loli(ctx, msg):
+  loli_conversa_token = os.environ.get('RAPIDAPIKEYLOLI')
+  boturl = "https://waifu.p.rapidapi.com/path"
+  
+  # Definindo caracteristicas especificas da convesa
+  user_id = ctx.author.id
+  de = ctx.author
+  para = "LOLI_BOTKKKKKKK"
+  situacao = "LOLI_BOTKKKKKKK é uma Loli e está presa em um porão."
 
-@client.command()
+  querystring = {"user_id":user_id,"message":msg,"from_name":de,"to_name":para,"situation":situacao,"translate_from":"pt","translate_to":"pt"}
+
+  payload = {}
+  headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Host": "waifu.p.rapidapi.com",
+    "X-RapidAPI-Key": loli_conversa_token
+  }
+  response = requests.request("POST", boturl, json=payload, headers=headers, params=querystring)
+  
+  await ctx.send(response.text)
+
+# Entra em um Canal de Voz e Diz um ohayo bem massa
+@client.command(pass_content = True)
+async def ohayo(ctx):
+  if ctx.author.voice:
+    channel = ctx.message.author.voice.channel
+    voice = await channel.connect()
+    source = FFmpegPCMAudio('ohayo.mp3')
+    player = voice.play(source)
+    sleep(9)
+    await ctx.guild.voice_client.disconnect()
+  else:
+    await ctx.send("Como que eu vou saber qual canal entrar? SEU TAPADO! entra em algum ai.")
+
+# Entra em um Canal de Voz e da um gemidão de anime
+@client.command(pass_content = True)
+async def gemido(ctx):
+  if ctx.author.voice:
+    channel = ctx.message.author.voice.channel
+    voice = await channel.connect()
+    source = FFmpegPCMAudio('gemido.mp3')
+    player = voice.play(source)
+    sleep(2)
+    await ctx.guild.voice_client.disconnect()
+  else:
+    await ctx.send("Como que eu vou saber qual canal entrar? SEU TAPADO! entra em algum ai.")
+
+# Sai de um Canal de voz
+@client.command(pass_content = True)
 async def leave(ctx):
-    await ctx.voice_client.disconnect()
+  if ctx.voice_client:
+    await ctx.guild.voice_client.disconnect()
+  else:
+    await ctx.send("Tô ai não! seu tobô!")
 
 
 client.run(os.environ.get('TOKEN'))
