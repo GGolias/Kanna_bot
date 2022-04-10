af63ae8b474a2a3f4035e497d034f5e38d11c12c
carlosneto726 <carlosneto726@gmail.com>
2022-04-09 21:08:28 -0300

    melhorei a parte de dar comandos

--- bot_loli.py
+++ bot_loli.py
@@ -1,21 +1,28 @@
 import discord
 import os
 from dotenv import load_dotenv, find_dotenv
+from discord.ext import commands
 
 load_dotenv(find_dotenv())
 
-client = discord.Client()
+client = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
     print("Logamos com sucesso como {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-      return
-      
-    if message.content.startswith('$') or message.content.startswith("$ajuda"):
-      await message.channel.send('**COMANDOS** \n$play \n$gemido \n$conversa \n$meme \n$loli')
+@client.command()
+async def comandos(ctx):
+  await ctx.send('**COMANDOS** \n$play \n$gemido \n$conversa \n$meme \n$loli')
+
+@client.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
 
 client.run(os.environ.get('TOKEN'))
